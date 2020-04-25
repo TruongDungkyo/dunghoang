dadfbf0d2c0dd500da2d8706b26c9ae45c1d3b0b
TruongDungkyo <62280421+TruongDungkyo@users.noreply.github.com>
2020-04-25 21:22:31 +0900

    Update 5.rb
    bai 5 hoàn chình

--- 5.rb
+++ 5.rb
@@ -1,17 +1,9 @@
-$n = 13
+$n = gets.to_i
 $h = $n/2
-
-
 
 if $n%2 == 1
     $m = ($n / 2) + 1
-    puts ("gia tri truc doi xung la #{$m}")
-else
-    $n%2 == 0
-    break
-    puts ("gia tri #{a} la so chan khong thuc hien duoc chuong trinh")
 end
-
 
 def print_or_not(x,y)
 
@@ -22,9 +14,9 @@ def print_or_not(x,y)
     end_below = $n - (y - $h) + 1 
     
 
-    if start_top <= x and x <= end_top and y <= $m and (x - start_top)%2 == 1
+    if start_top <= x and x <= end_top and y <= $m #and (x - start_top)%2 == 1
         result = "*"          
-    elsif start_below <= x and x <= end_below and $m <= y and (x - start_below)%2 == 1
+    elsif start_below <= x and x <= end_below and $m <= y #and (x - start_below)%2 == 1
         result = "*"
     else
         result = " "
@@ -34,10 +26,14 @@ end
 
 
 
-for i in 1..$n
-    for j in 1..$n
-        string = print_or_not(j,i)
-        print string
+if $n%2 == 1
+    for i in 1..$n
+        for j in 1..$n
+            string = print_or_not(j,i)
+            print string
+        end
+        print "\n"
     end
-    print "\n"
-end
+ else
+    puts "gia tri #{$n} la so chan khong the thuc thi"
+ end
