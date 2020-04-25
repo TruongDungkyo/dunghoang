e8da73f5698bb82bcca677d849c8517655241dd0
TruongDungkyo <62280421+TruongDungkyo@users.noreply.github.com>
2020-04-25 15:43:25 +0900

    Update 5.rb
    bo sung bai tap

--- 5.rb
+++ 5.rb
@@ -1,27 +1,30 @@
 $n = 13
-
 $h = $n/2
 
 
 
 if $n%2 == 1
     $m = ($n / 2) + 1
-    puts ("gia tri m la #{$m}")
+    puts ("gia tri truc doi xung la #{$m}")
 else
     $n%2 == 0
+    break
     puts ("gia tri #{a} la so chan khong thuc hien duoc chuong trinh")
 end
 
 
 def print_or_not(x,y)
-    max = $m + y - 1
-    max1 = $n - (y - $h) + 1 
-    min = $m - y + 1
-    min1 = y - $h
+
+    start_top = $m - y + 1   #diem bat dau in
+    end_top = $m + y - 1  #diem ket thuc in
+
+    start_below = y - $h
+    end_below = $n - (y - $h) + 1 
     
-    if min <= x and x <= max and y <= $m and (x - min)%2 == 1
-        result = "*"    
-    elsif min1 <= x and x <= max1 and $m <= y and (x - min1)%2 == 1
+
+    if start_top <= x and x <= end_top and y <= $m and (x - start_top)%2 == 1
+        result = "*"          
+    elsif start_below <= x and x <= end_below and $m <= y and (x - start_below)%2 == 1
         result = "*"
     else
         result = " "
@@ -29,15 +32,6 @@ def print_or_not(x,y)
     return result
 end
 
-
-
-for i in 1..$n
-    for j in 1..$n
-        string = print_or_not(j,i)
-        print string
-    end
-    print "\n"
-end
 
 
 for i in 1..$n
